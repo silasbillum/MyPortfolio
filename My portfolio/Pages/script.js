29724f2d3e2ec0fd9cb682629eba88725991383b
sila1400 <sila1400@edu.mercantec.dk>
2024-01-17 11:49:56 +0100

    cv

---
+++ My portfolio/Pages/script.js
@@ -0,0 +1,10 @@
+﻿// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('openMailButton').addEventListener('click', function () {
+        var sender_name = document.getElementById('sender_name').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+        window.location.href = 'mailto:stilhofsvej46@gmail.com?subject=' + sender_name + ' || ' + subject + '&body=' + message;
+    });
+});
